2bbf5bb996ed9f156c0c2dad3568e2c76232e7d5
chinamasters <1263641416@qq.com>
2022-07-17 22:15:55 +0800

    feat: git 命令

--- dev4.js
+++ dev4.js
@@ -2,5 +2,5 @@
 const name = "第一次提交";
 const names = "第2次提交";
 const name1s = "第3次提交12";
-// 我第一次修改了备注
-// 我还想提交一个备注 把上一次的commit合并为同一个
+// 我第一次修改了备注  git add .
+// 我还想提交一个备注 把上一次的commit合并为同一个  git commit --amend -m "这里填写提交新的注释"
