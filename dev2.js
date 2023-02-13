ee30fb3aacccbd4add5d0a62fba9c83b859aac1c
zhengtao <1263641416@qq.com>
2023-02-13 10:55:52 +0800

    feat: cherry-pick

--- dev2.js
+++ dev2.js
@@ -1,3 +1,22 @@
 // 创建dev2.js
 
 // dev2第2次提交
+
+/**
+ * Git dev2 commit
+ * dev second  commit
+ */
+
+/**
+ * Git dev2第一次提交
+ */
+
+/**
+ * Git dev2第二次提交
+ */
+
+/**
+ * Git dev2第三次提交
+ */
+
+// git cherry-pick one
